eba4eefcfecc580cfecd84aa36bd9f3c284cd36a
Yaggesh Likhar <yaggeshlikhar@localhost.localdomain>
2018-10-25 13:21:23 -0400

    Assignment 5 added

--- infrastructure/aws/assignment4/cloudformation/csye6225-aws-cf-create-application-stack.sh
+++ infrastructure/aws/assignment4/cloudformation/csye6225-aws-cf-create-application-stack.sh
@@ -1,15 +1,18 @@
 echo "please input the stack you want to create:"
 read l
 if [ -n "$l" ]
-privateSubnetReg=MyVPC-private-*
-vpc=$(aws ec2 describe-vpcs --query 'Vpcs[1].VpcId')
-privateSubnetId1=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$vpc" --filter "Name=tag:Name,Values=MyVPC-private*" --query 'Subnets[0].SubnetId')
-privateSubnetId2=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$vpc" --filter "Name=tag:Name,Values=MyVPC-private*" --query 'Subnets[1].SubnetId')
-privateSubnetId3=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$vpc" --filter "Name=tag:Name,Values=MyVPC-private*" --query 'Subnets[2].SubnetId')
 
+vpc=$(aws ec2 describe-vpcs --query 'Vpcs[0].VpcId')
+privateSubnetId1=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$vpc" --query 'Subnets[0].SubnetId')
+privateSubnetId2=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$vpc" --query 'Subnets[1].SubnetId')
+privateSubnetId3=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$vpc" --query 'Subnets[2].SubnetId')
+echo $vpc
+echo $privateSubnetId1
+echo $privateSubnetId2
+echo $privateSubnetId3
 then
 
-aws cloudformation create-stack --stack-name $l --template-body file://csye6225-cf-application.json --parameters ParameterKey=privateSubnetId1,ParameterValue=$privateSubnetId1 ParameterKey=privateSubnetId2,ParameterValue=$privateSubnetId2 ParameterKey=privateSubnetId3,ParameterValue=$privateSubnetId3
+aws cloudformation create-stack --stack-name $l --template-body file://csye6225-cf-application.json --parameters ParameterKey=privateSubnetId1,ParameterValue=$privateSubnetId1 ParameterKey=privateSubnetId2,ParameterValue=$privateSubnetId2 ParameterKey=privateSubnetId3,ParameterValue=$privateSubnetId3 ParameterKey=bucketname,ParameterValue="csye6225-fall2018-likhary.me.csye6225.com"
 
 else 
  echo "please input stack name!"
--- webapp/Application/Home/InterfaceController/FortranscationController.class.php
+++ webapp/Application/Home/InterfaceController/FortranscationController.class.php
@@ -5,57 +5,63 @@
  * Date: 10/13/2018
  * Time: 12:19
  */
-namespace Home\InterfaceController;
-use Common\Controller\CommonController;
-class FortranscationController extends CommonController {
 
-    public function  ifAuth($userid){
-        if (I('session.userid')!=$userid) {
-            $this->ajaxReturn(json_style(401, "no persiion", 10001));
+namespace Home\InterfaceController;
+
+use Common\Controller\CommonController;
+
+class FortranscationController extends CommonController
+{
+
+    public function ifAuth($userid)
+    {
+        if (I('session.userid') != $userid) {
+            $this->ajaxReturn(json_style(401, "no persiion", 10017));
         }
     }
 
-    public function ifTranscation($transactionid){
-        $tb_transcation = M('transcation');
+    public function ifTranscation($transactionid)
+    {
+        $tb_transcation = M('transaction');
         $where['id'] = $transactionid;
         $res = $tb_transcation->where($where)->find();
-        if (!isset($res)){
-            $this->ajaxReturn(json_style(400,"没有改交易",10001));
+        if (!isset($res)) {
+            $this->ajaxReturn(json_style(204, "no this transaction", 10013));
         }
         return $res;
     }
 
-    public  function  Uploadpicture(){
+    public function Uploadpicture()
+    {
         $upload = new \Think\Upload();//
-        $upload->maxSize   =     3145728 ;//
-        $upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');//
-//        $upload->saveName='';
-      //  $upload->replace='true';
-        $upload-> autoSub = 'false';
+        $upload->maxSize = 3145728;//
+        $upload->exts = array('jpg', 'gif', 'png', 'jpeg');//
+        $upload->autoSub = 'false';
         $upload->subName = null;
-        $upload->savePath  ='/receipts/';
-        $file =  $_FILES['attachfile'];
-        $info= $upload->uploadOne($file);
-        if(!$info){// 上传错误提示错误信息
-          //  $this->error();
-            $this->ajaxReturn(json_style(400,"error when upload file",10016));
-        }else{
+        $upload->savePath = '/receipts/';
+        $file = $_FILES['image'];
+        $info = $upload->uploadOne($file);
+        if (!$info) {// 上传错误提示错误信息
+         //   $this->error();
+         $this->ajaxReturn(json_style(400, "error when upload file,check your files perssion", 10016));
+        } else {
             return $info;
         }
     }
 
-    public  function  ifAttachment($attachmentid){
+    public function ifAttachment($attachmentid)
+    {
         $tb_receipt = M('receipt');
         $where['attachmentid'] = $attachmentid;
-        $res =$tb_receipt->where($where)->find();
-        if (isset($res)){
+        $res = $tb_receipt->where($where)->find();
+        if (isset($res)) {
             //删除文件 on s3
             deleteons3($res['localurl']);
             //删除本地文件
             unlink($res['localurl']);
-        return $res;
-        }else{
-            $this->ajaxReturn(json_style(400,"没有该文件",10001));
+            return $res;
+        } else {
+            $this->ajaxReturn(json_style(204, "this transaction does not have this attachment file", 10018));
         }
     }
 
